335114bce9fa0524a0272a1006632b78d77a558d
rudu-cell <rdrsaraswat@gmail.com>
2026-03-26 13:41:38 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
+import pandas as pd
 
-app =FastAPI()
+app = FastAPI()
 
 @app.get("/")
-
 def home():
-    return "Hello Niggas!"
+    return {"message": "Hello FastAPI"}
 
+@app.get("/get-csv-data")
+def get_csv_data():
+
+    try:
+        df = pd.read_csv("students_complete.csv")
+
+        return {
+            "columns": list(df.columns),
+            "total_rows": len(df),
+            "data": df.fillna("").to_dict(orient="records")
+        }
+
+    except FileNotFoundError:
+        return {
+            "error": "students_complete.csv not found. Make sure it is in same folder as app.py"
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
